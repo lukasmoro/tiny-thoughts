ba09f7a11ca9e83a3dbf8f3a25aff44cd606cf09
Lukas Moro <lukas.moro@me.com>
2025-04-02 13:00:05 +0200

    removed model again

--- TinyThoughts/Models/Item.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-import CoreData
-
-@objc(Item)
-public class Item: NSManagedObject {
-    @NSManaged public var timestamp: Date?
-} 
--- TinyThoughts/Models/ThoughtTagRelation.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import CoreData
-
-@objc(ThoughtTagRelation)
-public class ThoughtTagRelation: NSManagedObject {
-    @NSManaged public var id: UUID
-    @NSManaged public var tag: Tag?
-    @NSManaged public var thought: Thought?
-} 
